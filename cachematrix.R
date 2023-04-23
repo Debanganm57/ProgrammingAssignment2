e266a37eacfa723ca14344e104ca51b4a1ee3a34
Debanganm57 <130385375+Debanganm57@users.noreply.github.com>
2023-04-23 22:15:33 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,18 @@
 
 ## Write a short comment describing this function
 
+#The makeCacheMatrix() function takes a single argument x, which is a matrix. If no matrix is provided, an empty matrix is used as the default value.
+
+#The next block defines a function called get() that retrieves the matrix from the cache environment using the $ operator on the cache object.
+
+#The set() function is defined to set the matrix in the cache environment. It takes a single argument m that represents the matrix to be cached. The cache$i variable is set to NULL to invalidate the cached inverse, because the matrix has changed.
+
+#The getInverse() function is defined to retrieve the cached inverse from the cache environment using the $ operator on the cache object.
+
+#The setInverse() function is defined to cache the inverse in the cache environment. It takes a single argument i that represents the inverse to be cached.
+
+#Finally, the makeCacheMatrix() function returns a list of functions that can be used to interact with the cached matrix and its cached inverse.
+
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   
@@ -28,6 +40,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+#The cacheSolve() function takes a single argument x, which is a makeCacheMatrix() object created using the makeCacheMatrix() function.
+
+#The first if statement checks if the cached inverse is available using the getInverse() method of the makeCacheMatrix() object. If the cached inverse is available, a message is printed indicating that the cached value is being used, and the cached inverse is returned using the getInverse() method.
+
+#If the cached inverse is not available, the else block is executed. The solve() function is used to compute the inverse of the matrix, which is stored in the inverse variable.
+
+#The setInverse() method of the makeCacheMatrix() object is then used to cache the computed inverse.
+
 
 cacheSolve <- function(x) {
   
